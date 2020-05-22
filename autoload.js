73f98b102dc413ba9d20e4e751d74966bc5a98c7
517487575 <517487575@qq.com>
2020-05-23 00:44:29 +0800

    测试 v1.0.7

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/517487575/live2d-widget@v1.0.6/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/517487575/live2d-widget@v1.0.7/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
